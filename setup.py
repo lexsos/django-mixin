de423cb8ed2e23648c76c0a402c5baa8d77daf04
lexsos <ale-sosnin@yandex.ru>
2013-11-03 08:41:48 +0400

    Add AdminTinymce

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-mixin',
+    version='0.1',
+    description='Mixin for django projects',
+    author='lexsos',
+    author_email='ale-sosnin@yandex.ru',
+    packages=find_packages(),
+)
