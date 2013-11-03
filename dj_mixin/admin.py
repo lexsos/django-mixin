1e2d24da66f35d88f36ac94faa0f9edb9a63004e
lexsos <ale-sosnin@yandex.ru>
2013-11-03 09:04:13 +0400

    Add EnabledMixin

--- dj_mixin/admin.py
+++ dj_mixin/admin.py
@@ -1,7 +1,8 @@
-from  tinymce.widgets import AdminTinyMCE
+from django.utils.translation import ugettext_lazy as _
+from tinymce.widgets import AdminTinyMCE
 
 
-class AdminTinymce(object):
+class AdminTinymceMixin(object):
 
     rich_fields = []
 
@@ -11,4 +12,27 @@ class AdminTinymce(object):
     def formfield_for_dbfield(self, db_field, **kwargs):
         if db_field.name in self.get_rich_fields():
             kwargs['widget'] = AdminTinyMCE()
-        return super(AdminTinymce, self).formfield_for_dbfield(db_field, **kwargs)
+        return super(AdminTinymceMixin, self).formfield_for_dbfield(db_field, **kwargs)
+
+
+class EnabledMixin(object):
+
+    def make_enabled(self, request, queryset):
+        queryset.update(enabled=True)
+
+    def make_disabled(self, request, queryset):
+        queryset.update(enabled=False)
+
+    def get_actions(self, request):
+        actions = super(EnabledMixin, self).get_actions(request)
+        if not 'make_enabled' in actions:
+            action = (self.make_enabled,
+                      'make_enabled',
+                      _('Enable selected %(verbose_name_plural)s'))
+            actions['make_enabled'] = action
+        if not 'make_disabled' in actions:
+            action = (self.make_disabled,
+                      'make_disabled',
+                      _('Disable selected %(verbose_name_plural)s'))
+            actions['make_disabled'] = action
+        return actions
