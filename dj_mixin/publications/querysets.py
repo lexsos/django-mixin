143924be72df0c39c0dfddbabafb2c23647103ca
lexsos <ale-sosnin@yandex.ru>
2013-12-05 10:10:16 +0400

    Fix code style

--- dj_mixin/publications/querysets.py
+++ dj_mixin/publications/querysets.py
@@ -25,7 +25,7 @@ class PublicationQuerySet(query.QuerySet):
 
     def _published_query(self):
         now = timezone.now()
-        q = Q(pub_date_end__gte=now)|Q(pub_date_end=None)
+        q = Q(pub_date_end__gte=now) | Q(pub_date_end=None)
         q &= Q(pub_date_start__lte=now)
         q &= Q(enabled=True)
         return q
