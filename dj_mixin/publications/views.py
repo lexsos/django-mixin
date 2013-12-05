511f9529a8f2ecc71f9057a03e5d87d933e1550b
lexsos <ale-sosnin@yandex.ru>
2013-12-05 10:04:37 +0400

    Add SortMixin to publications

--- dj_mixin/publications/views.py
+++ dj_mixin/publications/views.py
@@ -11,7 +11,32 @@ class PublishedMixin(object):
         return queryset.published()
 
 
-class PublicationListView(PublishedMixin, ListView):
+class SortMixin(object):
+
+    def _get_sort_key(self):
+        return self.request.GET.get('sort', 'default')
+
+    def _get_sort_tuple(self):
+        sort_key = self._get_sort_key()
+        sort_orders = getattr(self, 'sort_orders', {})
+        if not sort_key in sort_orders:
+            return None
+        return sort_orders[sort_key]
+
+    def get_queryset(self):
+        queryset = super(SortMixin, self).get_queryset()
+        sort_tuple = self._get_sort_tuple()
+        if sort_tuple is None:
+            return queryset
+        return queryset.order_by(*sort_tuple)
+
+    def get_context_data(self, **kwargs):
+        context = super(SortMixin, self).get_context_data(**kwargs)
+        context['sort'] = self._get_sort_key()
+        return context
+
+
+class PublicationListView(PublishedMixin, SortMixin, ListView):
     """Returns only published records."""
 
 
